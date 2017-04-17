8dd84b13d7f252b1dab26c4c8599399a284f8cd8
vpoiaghi <vincent.poiaghi@free.fr>
2017-04-17 14:27:22 +0200

    Add files via upload
    Première version découpée en applications distinctes (commit 1)

---
+++ CardMasterViewer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("CardMasterViewer")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("CardMasterViewer")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("c391c885-83ef-4f5b-a2c1-932a299615eb")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ CardMasterViewer/Viewer.vb
@@ -0,0 +1,18 @@
+﻿Imports CardMasterCard.Card
+Imports CardMasterImageBuilder
+Imports System.Windows.Forms
+
+Public Class Viewer
+    Inherits PictureBox
+
+    Private m_cardDrawer As New Drawer
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+    Public Sub ShowCard(card As Card)
+        Me.Image = m_cardDrawer.DrawCard(card)
+    End Sub
+
+End Class
