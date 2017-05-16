5e83e1626c62b953a103610553afc08295722070
Bruno Bassac <b.bassac@lectra.com>
2017-05-16 14:29:33 +0200

    feat: citation alignment fine

--- CardMasterImageBuilder/SkinFactory.cs
+++ CardMasterImageBuilder/SkinFactory.cs
@@ -93,6 +93,7 @@ namespace CardMasterImageBuilder
                 skinElement = new SETextArea(skin, 35, 380, w - 100, 140, "<Citation>");
                 ((SETextArea)skinElement).TextAttribute = "Citation";
                 ((SETextArea)skinElement).TextVerticalAlign = VerticalAlignment.Center;
+                ((SETextArea)skinElement).TextAlign = HorizontalAlignment.Center;
                 ((SETextArea)skinElement).TextFont = new Font("Arial", 10, FontStyle.Italic); 
                 skin.Elements.Add(skinElement);
             }
