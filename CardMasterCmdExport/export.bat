198608d1389451da5d0fdca62ee4e8e78e89fe4a
Bruno Bassac <b.bassac@lectra.com>
2017-06-01 17:02:52 +0200

    feat: update export.bat to export all single cards to debug/bin/Export

--- CardMasterCmdExport/export.bat
+++ CardMasterCmdExport/export.bat
@@ -1,3 +1,3 @@
 rmdir /Q/S bin\Debug\Export
 mkdir bin\Debug\Export
-bin\Debug\CardMasterCmdExport.exe ..\data\Naruto.json bin\Debug\Export /m:board /f:PNG
+bin\Debug\CardMasterCmdExport.exe ..\data\Naruto.json bin\Debug\Export /m:all /backside /f:PNG
