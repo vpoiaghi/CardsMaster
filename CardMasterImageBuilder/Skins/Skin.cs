e33e9ffa3c3614bc43023def037ede46f7d11069
Bruno Bassac <b.bassac@lectra.com>
2017-06-13 11:00:35 +0200

    removed unnecessay attributes

--- CardMasterImageBuilder/Skins/Skin.cs
+++ CardMasterImageBuilder/Skins/Skin.cs
@@ -6,24 +6,21 @@ namespace CardMasterImageBuilder.Skins
     {
         public int Width { get; set; }
         public int Height { get; set; }
-        public DirectoryInfo ResourcesDirectory { get; set; }
         public SkinElements Elements { get; set; }
 
 
         protected Skin()
         { }
 
-        public Skin(int width, int height, DirectoryInfo resourcesDirectory)
+        public Skin(int width, int height)
         {
             this.Width = width;
             this.Height = height;
-            this.ResourcesDirectory = resourcesDirectory;
             this.Elements = new SkinElements();
         }
 
         ~Skin()
         {
-            this.ResourcesDirectory = null;
             this.Elements.Clear();
         }
 
